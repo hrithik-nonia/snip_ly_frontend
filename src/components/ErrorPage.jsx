806a38fc3861556130f44f8e6d4ab396bfe47703
Hrithik <hrithiknonia66@gmail.com>
2026-08-23 16:12:12 +0530

    fix: add vercel.json for SPA routing

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,10 @@
 // built in imports
 import { AlarmClock, Link2, HeartOff } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 
 // custom imports
 import { CommonButton } from "./CommonComponents";
 
 export default function ErrorPage({ pathname }) {
-  const navigate = useNavigate();
   return (
     <div className="flex min-h-[500px] w-full flex-col items-center justify-center p-6 text-center">
       {pathname === "/linkExpiry410Page" ? (
@@ -46,12 +44,7 @@ export default function ErrorPage({ pathname }) {
       {/* Action Buttons */}
       <div className="mt-8 flex w-full max-w-xs flex-col gap-3">
         {/* Go to Homepage Button */}
-        <CommonButton
-          isBgColor={true}
-          title="Go to Homepage"
-          isIcon={true}
-          onclick={() => navigate("/")}
-        />
+        <CommonButton isBgColor={true} title="Go to Homepage" isIcon={true} />
 
         {/* Create Your Own Link Button */}
         <CommonButton
